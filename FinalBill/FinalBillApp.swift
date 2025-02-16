2fd120657a119596bee498d00dc4b7fa26b5b411
RolandGeorge <154392091+RolandGeorge@users.noreply.github.com>
2025-02-16 21:12:12 +1100

    Initial Commit

---
+++ FinalBill/FinalBillApp.swift
@@ -0,0 +1,32 @@
+//
+//  FinalBillApp.swift
+//  FinalBill
+//
+//  Created by Roland on 16/2/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FinalBillApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FinalBillTests/FinalBillTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalBillTests.swift
+//  FinalBillTests
+//
+//  Created by Roland on 16/2/2025.
+//
+
+import Testing
+@testable import FinalBill
+
+struct FinalBillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
